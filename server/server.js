49940a0d72b80bf8cd0e68bd95876d29c4256bea
bbandhu <bandhavi.b12@gmail.com>
2023-07-13 15:52:16 -0700

    setting up apollo server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
+const {ApolloServer} = require('apollo-server-express');
+const {typeDefs, resolvers} = require('./schemas');
 const routes = require('./routes');
 
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,5 +23,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })  
 });
